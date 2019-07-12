4a6bbcff57af2e02690a51690a8f69aee7fe9e4b
Thien Hoang Ngoc <ngoc@tbo.de>
2019-07-12 11:33:20 +0200

    Changed the data models for adapter

--- app/src/main/java/com/example/sportassistantapp/recyclerViewAdapter/TrainingDayAdapter.java
+++ app/src/main/java/com/example/sportassistantapp/recyclerViewAdapter/TrainingDayAdapter.java
@@ -1,22 +1,35 @@
 package com.example.sportassistantapp.recyclerViewAdapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sportassistantapp.R;
 import com.example.sportassistantapp.recyclerViewItemModels.ExerciseItem;
 
 import java.util.ArrayList;
 
 public class TrainingDayAdapter extends RecyclerView.Adapter<TrainingDayAdapter.ItemRowHolder> {
 
+    private ArrayList<ExerciseItem> dataList;
+    private Context context;
+
+    public TrainingDayAdapter(ArrayList<ExerciseItem> dataList, Context context) {
+        this.dataList = dataList;
+        this.context = context;
+    }
+
+
     @NonNull
     @Override
     public ItemRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.create_training_program_day_list_item, null);
+        ItemRowHolder itemRowHolder = new ItemRowHolder(itemView);
+        return itemRowHolder;
     }
 
     @Override
@@ -36,8 +49,7 @@ public class TrainingDayAdapter extends RecyclerView.Adapter<TrainingDayAdapter.
         }
     }
 
-    private ArrayList<ExerciseItem> dataList;
-    private Context context;
+
 
     public TrainingDayAdapter(Context context, ArrayList<ExerciseItem> dataList){
         this.context = context;
--- app/src/main/java/com/example/sportassistantapp/recyclerViewItemModels/TrainingDayItem.java
+++ app/src/main/java/com/example/sportassistantapp/recyclerViewItemModels/TrainingDayItem.java
@@ -4,14 +4,25 @@ import java.util.ArrayList;
 
 public class TrainingDayItem {
 
+    private String workOutDayTitle;
     private ArrayList<ExerciseItem> itemList;
 
-    public TrainingDayItem(){
+
+    public TrainingDayItem() {
 
     }
 
-    public TrainingDayItem(ArrayList<ExerciseItem> itemList) {
+    public TrainingDayItem(String workOutDayTitle, ArrayList<ExerciseItem> itemList) {
+        this.workOutDayTitle = workOutDayTitle;
         this.itemList = itemList;
+    }
+
+    public String getWorkOutDayTitle() {
+        return workOutDayTitle;
+    }
+
+    public void setWorkOutDayTitle(String workOutDayTitle) {
+        this.workOutDayTitle = workOutDayTitle;
     }
 
     public ArrayList<ExerciseItem> getItemList() {
